7a3dfe2cb0ac93848d19f1f3e412d9fd625289ff
aravind.ln <aravind.ln@gavstech.com>
2023-10-27 21:26:42 +0530

    commit

---
+++ src/app/UserData.ts
@@ -0,0 +1,7 @@
+export class UserData{
+    FirstName:string="Aravind";
+    LastName:string="LN";
+    location:string="Chennai";
+    company:string="Gavs";
+    
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserData } from './UserData';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  udata:UserData=new UserData();
+  userList:UserData[]=[];
   title = 'bindings';
+  themeName:string='forest';
+  applyColor:string="blue";
+  size: number=100;
+  stateName:string="TN";
+ 
+  enableDisable:boolean=false;
+  saveData()
+  {
+    
+    this.userList.push(this.udata);
+    console.log('Added data to the array...');
+    console.log(this.udata);
+    this.udata=new UserData();
+  }
+
+  checkData()
+  {
+   //console.log(event.target);
+   console.log(this.udata.FirstName);
+   
+  }
+  sendData()
+  {
+    console.log("Sending data to Express backend.");
+  }
 }
